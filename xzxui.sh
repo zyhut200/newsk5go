0c0bebfffb84a8326595a29125b502c372ea7857
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-11-06 16:55:49 +0800

    Create xzxui.sh

---
+++ xzxui.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect
+
+# 设置超时时间，根据需要调整
+set timeout 30
+
+# 启动x-ui卸载脚本
+spawn x-ui uninstall
+
+# 等待卸载脚本的特定提示
+expect "Are you sure you want to uninstall x-ui? (yes/no)"
+
+# 自动响应 "yes"
+send "yes\r"
+
+# 等待任何其他可能的提示，并相应地发送响应
+# ...
+
+# 结束expect脚本
+expect eof
